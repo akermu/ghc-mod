9a53f7f472b04fc8c67242af6075e8e51b6658bf
Alan Zimmerman <alan.zimm@gmail.com>
2016-07-17 16:00:04 +0200

    Add test case demonstrating #811
    Using GHC 7.10.3 and current ghc-mod master, the packageKey shows up as
    "main@main"
    
    It should be a random string

--- test/TargetSpec.hs
+++ test/TargetSpec.hs
@@ -4,15 +4,24 @@ module TargetSpec where
 import Language.Haskell.GhcMod.Target
 import Language.Haskell.GhcMod.LightGhc
 import Language.Haskell.GhcMod.Gap
+import Language.Haskell.GhcMod.Logger
+
 import Test.Hspec
 
 import TestUtils
+import Dir
 
 import GHC
 import Data.List
 import Data.Maybe
 import System.Directory
 import System.FilePath
+import System.Process
+
+import qualified DynFlags      as GHC
+import qualified GHC           as GHC
+import qualified Module        as GHC
+import qualified Outputable    as GHC
 
 spec :: Spec
 spec = do
@@ -45,3 +54,32 @@ spec = do
             x <- withLightHscEnv [] $ \env -> runD $ do
                 resolveModule env srcDirs (Left $ dir </> "test/data/target/Cpp.hs")
             liftIO $ x `shouldBe` Just (ModulePath "Cpp" $ dir </> "test/data/target/Cpp.hs")
+
+    describe "runGmlT" $ do
+        it "main module import packageKey should not be 'main@main'" $ do
+            withDirectory_ "test/data/cabal-packagekey" $ do
+                res <- runD $ checkPackageKeys
+                print res
+                res `shouldBe` []
+
+checkPackageKeys :: IOish m => GhcModT m [String]
+checkPackageKeys =
+    runGmlTWith
+      [Left "./src/Main.hs"]
+      return
+      id
+      action
+  where
+    action = do
+      graph  <- GHC.getModuleGraph
+      let modSum = head graph
+      setGhcContext modSum
+      gnames <- GHC.getNamesInScope
+      liftIO $ putStrLn $ showGhcQual $ map GHC.nameModule gnames
+      return $ (filter ("main@main" `isPrefixOf`) (map (showGhcQual . GHC.nameModule) gnames))
+
+showGhcQual :: (GHC.Outputable a) => a -> String
+showGhcQual x = GHC.showSDocForUser GHC.unsafeGlobalDynFlags GHC.alwaysQualify $ GHC.ppr x
+setGhcContext :: GHC.GhcMonad m => GHC.ModSummary -> m ()
+
+setGhcContext modSum = GHC.setContext [GHC.IIModule (GHC.moduleName $ GHC.ms_mod modSum)]
